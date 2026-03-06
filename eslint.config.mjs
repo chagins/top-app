16bd818e72aeeeb806271a6a66dd7a67f1b9e314
chagins <chagin_yav@hotmail.com>
2026-03-06 12:00:38 +0500

    feat: init project settings

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier/flat';
+import tseslint from 'typescript-eslint';
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  ...tseslint.configs.strictTypeChecked,
+  prettier,
+  {
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'error',
+    },
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    'eslint.config.mjs',
+    'postcss.config.mjs',
+  ]),
+]);
+
+export default eslintConfig;
